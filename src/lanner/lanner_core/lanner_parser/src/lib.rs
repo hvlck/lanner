7e17f37bd00e66827de5c1a29b269147c9afd57c
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-06-27 18:57:16 -0400

    feat(lanner_core/lanner_parser): expression struct

--- src/lanner/lanner_core/lanner_parser/src/lib.rs
+++ src/lanner/lanner_core/lanner_parser/src/lib.rs
@@ -1,7 +1,26 @@
+pub struct Expression {
+    pub original: String,
+    pieces: Vec<char>,
+    total: f64
+}
+
+impl Expression {
+    pub fn new(exp: String) -> Expression {
+        let mut pieces = Vec::new();
+
+        for character in exp.chars() {
+            pieces.push(character);
+        }
+
+        Expression {
+            original: exp,
+            pieces: pieces
+            total: 0.0
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    use super::*;
 }
