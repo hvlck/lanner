3f7b8a01d9167a8d2638a6906294640b2dfae985
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-06-27 17:58:52 -0400

    feat(lanner-core): add function

--- src/lanner/lanner-core/src/lib.rs
+++ src/lanner/lanner-core/src/lib.rs
@@ -1,7 +1,16 @@
+pub fn add(nums: Vec<f64>) -> f64 {
+    let mut sum: f64 = 0.0;
+    for number in nums.iter() {
+        sum += number;
+    }
+    sum
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn check_add() {
+        assert_eq!(add(vec![2.2, 2.2]), 4.4);
     }
 }
