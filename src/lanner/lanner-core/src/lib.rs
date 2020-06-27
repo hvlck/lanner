6f58360fb9ef3497f3e22625fb7dac34d0be4691
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-06-27 18:05:20 -0400

    feat(lanner-core): subtract function

--- src/lanner/lanner-core/src/lib.rs
+++ src/lanner/lanner-core/src/lib.rs
@@ -6,11 +6,27 @@ pub fn add(nums: Vec<f64>) -> f64 {
     sum
 }
 
+pub fn subtract(nums: Vec<f64>) -> f64 {
+    let mut diff = nums[0];
+    let mut subiter = nums.iter();
+    
+    subiter.next();
+    for number in subiter {
+        diff -= number;
+    }
+    diff
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn check_add() {
+        assert_eq!(add(vec![10.0000025, 10.00001]), 20.0000125);
         assert_eq!(add(vec![2.2, 2.2]), 4.4);
+    }
+    #[test]
+    fn check_subtract() {
+        assert_eq!(subtract(vec![2.2, 2.2]), 0.0);
     }
 }
