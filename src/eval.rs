bca6e2ad7cdfee0fa1ebf6242ebd53fd44622c25
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-12-31 20:48:26 -0500

    fix(core): fix multiplication dividing
    I was rather careless when writing the piece of code that actualyl evaluated an expression, and so used division for multiplication and division operations instead of just for division operations.

--- src/eval.rs
+++ src/eval.rs
@@ -3,22 +3,12 @@ use crate::{AstNode, Operation};
 pub fn evaluate_simple(node: AstNode) -> Result<f64, ()> {
     match node {
         AstNode::Expression { lhs, rhs, operator } => match operator {
-            Operation::Add => {
-                Ok(lhs + rhs)
-            },
-            Operation::Subtract => {
-                Ok(lhs - rhs)
-            },
-            Operation::Divide => {
-                Ok(lhs / rhs)
-            },
-            Operation::Multiply => {
-                Ok(lhs / rhs)
-            },
-            Operation::Exponent => {
-                Ok(lhs.powf(rhs))
-            },
-            _=> Err(())
+            Operation::Add => Ok(lhs + rhs),
+            Operation::Subtract => Ok(lhs - rhs),
+            Operation::Divide => Ok(lhs / rhs),
+            Operation::Multiply => Ok(lhs * rhs),
+            Operation::Exponent => Ok(lhs.powf(rhs)),
+            _ => Err(()),
         },
         _ => Err(()),
     }
