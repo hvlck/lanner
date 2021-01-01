881411b12be4958f3ebd552d9d399303c027273a
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-12-31 19:14:32 -0500

    feat: basic expression, operator, and function parsing; tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
+#[macro_use]
+// std
+
+// crates
+use pest::Parser;
+use pest_derive::*;
+
+// local
+
+#[derive(Parser)]
+#[grammar = "lib.pest"]
+pub struct LannerParser;
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn check_number() {
+        let whole = LannerParser::parse(Rule::number, "10");
+        assert!(whole.is_ok());
+        let integer = LannerParser::parse(Rule::number, "10.15");
+        assert!(integer.is_ok());
+    }
+
+    #[test]
+    fn check_operators() {}
+
+    #[test]
+    fn check_expression() {
+        let exp = LannerParser::parse(Rule::expression, "10+10");
+        assert!(exp.is_ok());
+
+        let exp_with_spaces = LannerParser::parse(Rule::expression, "10 + 10");
+        assert!(exp_with_spaces.is_ok());
     }
 }
